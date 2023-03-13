ad6f1861d2c296c9caec8d89bad21e93dab12c30
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-13 21:22:01 +0500

    new2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var a;
-a = 2;
-console.log("gggg",a);
+{
+    var tempc = prompt("Enter tempreture in c");
+    var tempf = (tempc*9/5)+32;
+    console.log("tempreture in f:",tempf);
+}
+{
+    var a,b,c;
+    a =500;
+    b =50;
+    c =a+b;
+    console.log("the value of c answer is:",c);
+
+}
