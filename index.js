ecee17062b0c0b369f0317f3bfbd43ac357cc6a2
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-25 19:13:50 +0500

    new2

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+// {
+//     var tempc = prompt("Enter tempreture in c");
+//     var tempf =(tempc*9/5)+32;
+//     console.log("tempreture in f",tempf);
+// }
+// {
+//     var a,b,c,d,e,f;
+//     a=8;
+//     b=12;
+//     c=22;
+//     d=34;
+//     e=99;
+//     f = b*c/a+(e-d);
+//     console.log("the answer f is :",f);
+// }
 {
-    var tempc = prompt("Enter tempreture in c");
-    var tempf =(tempc*9/5)+32;
-    console.log("tempreture in f",tempf);
-}
-{
-    var a,b,c,d,e,f;
-    a=8;
-    b=12;
-    c=22;
-    d=34;
-    e=99;
-    f = b*c/a+(e-d);
-    console.log("the answer f is :",f);
+    var usermarks = prompt("Enter Your Marks");
+    if (usermarks < 50)
+    {
+      console.log("You are fail");
+    }
+   else if (usermarks < 60)
+    {
+      console.log("Your Grade is D.");
+    }
+    else if (usermarks < 70)
+    {
+      console.log("Your Grade is c.");
+    }
+    else if (usermarks < 80)
+    {
+      console.log("Your Grade is B.");
+    }
+    else if (usermarks < 90)
+    {
+      console.log("Your Grade is A.");
+    }
+    else if (usermarks < 100)
+    {
+      console.log("Your Grade is A+.");
+    }
 }
