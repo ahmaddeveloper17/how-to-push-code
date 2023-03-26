5b423840b2347429f5efea1df2fda60b16c7f3a5
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-26 14:48:17 +0500

    new2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// {
+//1{
 //     var tempc = prompt("Enter tempreture in c");
 //     var tempf =(tempc*9/5)+32;
 //     console.log("tempreture in f",tempf);
 // }
-// {
+// 2{
 //     var a,b,c,d,e,f;
 //     a=8;
 //     b=12;
@@ -13,30 +13,47 @@
 //     f = b*c/a+(e-d);
 //     console.log("the answer f is :",f);
 // }
+// 3{
+//     var usermarks = prompt("Enter Your Marks");
+//     if (usermarks < 50)
+//     {
+//       console.log("You are fail");
+//     }
+//    else if (usermarks < 60)
+//     {
+//       console.log("Your Grade is D.");
+//     }
+//     else if (usermarks < 70)
+//     {
+//       console.log("Your Grade is c.");
+//     }
+//     else if (usermarks < 80)
+//     {
+//       console.log("Your Grade is B.");
+//     }
+//     else if (usermarks < 90)
+//     {
+//       console.log("Your Grade is A.");
+//     }
+//     else if (usermarks < 100)
+//     {
+//       console.log("Your Grade is A+.");
+//     }
+// }
+//  4{
+//     var year = prompt("Enter a Year");
+//     if (year%4==0)
+//     {
+//         console.log("This Year is a Leap Year.");
+//     }
+//     else 
+//     {
+//         console.log("This year is not a Leap Year.");
+//     }
+//  }
 {
-    var usermarks = prompt("Enter Your Marks");
-    if (usermarks < 50)
-    {
-      console.log("You are fail");
-    }
-   else if (usermarks < 60)
-    {
-      console.log("Your Grade is D.");
-    }
-    else if (usermarks < 70)
-    {
-      console.log("Your Grade is c.");
-    }
-    else if (usermarks < 80)
-    {
-      console.log("Your Grade is B.");
-    }
-    else if (usermarks < 90)
-    {
-      console.log("Your Grade is A.");
-    }
-    else if (usermarks < 100)
-    {
-      console.log("Your Grade is A+.");
-    }
+    // yeh program user ko welcome message show kare ga
+let name = prompt("Apna naam likhiye:"); // user se naam mangna
+console.log("Assalam o Alaikum, " + name + "!"); // welcome message show karna
+
 }
