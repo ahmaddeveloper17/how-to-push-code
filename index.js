4faa17b160af1875323cd31d39269806e9af28ea
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-03 22:29:17 +0500

    new2

--- index.js
+++ index.js
@@ -1,100 +1,11 @@
-//1{
-//     var tempc = prompt("Enter tempreture in c");
-//     var tempf =(tempc*9/5)+32;
-//     console.log("tempreture in f",tempf);
-// }
-// 2{
-//     var a,b,c,d,e,f;
-//     a=8;
-//     b=12;
-//     c=22;
-//     d=34;
-//     e=99;
-//     f = b*c/a+(e-d);
-//     console.log("the answer f is :",f);
-// }
-// 3{
-//     var usermarks = prompt("Enter Your Marks");
-//     if (usermarks < 50)
-//     {
-//       console.log("You are fail");
-//     }
-//    else if (usermarks < 60)
-//     {
-//       console.log("Your Grade is D.");
-//     }
-//     else if (usermarks < 70)
-//     {
-//       console.log("Your Grade is c.");
-//     }
-//     else if (usermarks < 80)
-//     {
-//       console.log("Your Grade is B.");
-//     }
-//     else if (usermarks < 90)
-//     {
-//       console.log("Your Grade is A.");
-//     }
-//     else if (usermarks < 100)
-//     {
-//       console.log("Your Grade is A+.");
-//     }
-// }
-//  4{
-//     var year = prompt("Enter a Year");
-//     if (year%4==0)
-//     {
-//         console.log("This Year is a Leap Year.");
-//     }
-//     else 
-//     {
-//         console.log("This year is not a Leap Year.");
-//     }
-//  }
-// {
-//     // yeh program user ko welcome message show kare ga
-// let name = prompt("Apna naam likhiye:"); // user se naam mangna
-// console.log("Assalam o Alaikum, " + name + "!"); // welcome message show karna
+function evenodd() {
+  var input = document.getElementById("number").value
+  var para = document.getElementById("para")
+  console.log("para",para);
+  if (input%2==0) {
+   para.innerHtml="number is even"
+  }
+  else
+  para.innerHtml="number is odd"
+}
 
-// }
-var vowl="k"
-if(vowl=="a")
-     console.log("Your entered word is vowl");
-else if(vowl=="e")
-     console.log("Your entered word is vowl");
-else if(vowl=="i")
-     console.log("Your entered word is vowl");
-else if(vowl=="o")
-     console.log("Your entered word is vowl");
-else if(vowl=="u")
-     console.log("Your entered word is vowl");
-else if(vowl=="A")
-     console.log("Your entered word is vowl");
-else if(vowl=="E")
-     console.log("Your entered word is vowl");
-else if(vowl=="I")
-     console.log("Your entered word is vowl");
-else if(vowl=="O")
-     console.log("Your entered word is vowl");
-else if(vowl=="U")
-     console.log("Your entered word is vowl");
-else
-    console.log("Your entered word is not a vowl");
-    {
-     var  Physics=95, Chemistry=95, Biology=95, Mathematics=95, Computer=95;
-     var Totalnumber = "95%" ;
-     if (Totalnumber > "90%" )
-        console.log("Grade A");
-     else if (Totalnumber > "80%" )
-        console.log("Grade B");
-     else if (Totalnumber > "70%" )
-        console.log("Grade C");
-     else if (Totalnumber > "60%" )
-        console.log("Grade D");
-     else if (Totalnumber > "50%" )
-        console.log("Grade E");
-     else if (Totalnumber > "40%" )
-        console.log("Grade F");
-     else if (Totalnumber < "40%" )
-        console.log("Failed");
- }   
