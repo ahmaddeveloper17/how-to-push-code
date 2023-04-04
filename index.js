8f6de62fdbf8a3be4225d7a049814a8991edf551
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-04 17:03:16 +0500

    new2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+console.log(document.getElementById("heading"));
+var heading = document.getElementById("heading")
 function evenodd() {
   var input = document.getElementById("number").value
   var para = document.getElementById("para")
-  console.log("para",para);
-  if (input%2==0) {
+ 
+  if (input.value % 2 == 0) {
    para.innerHtml="number is even"
   }
   else
-  para.innerHtml="number is odd"
+ { para.innerHtml="number is odd"}
 }
 
