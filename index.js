347ad8567d456a239e818c3341a052fb4c721986
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-06 11:50:23 +0500

    new2

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-var username= prompt ("Enter your name");
-document.write("Welcome to my website " + username);
+// var username= prompt ("Enter your name");
+// document.write("Welcome to my website " + username);
+
+// let Number = 6 ** 3
+// console.log(Number);
+
+// let username = prompt ("Enter your name")
+// document.write = data.reduces ("e")
+// let usernum1 = prompt("enter first number to be added")
+// let usernum2 = prompt("enter second number to be added")
+// let result = usernum1 + usernum2;
+// console.log(result);
+// let table = +prompt ("Which number of table");
+// for (let index = 1; index <= 15; index++) {
+//     console.log( table +" x "+ index + "=" + table*index  );
+    
+// }
+
+// let userage = +prompt ("Enter your age.")
+// if (userage <= 10)
+// {
+//     console.log("Welcome.");
+// }
+// else
+// {
+//     console.log("You are not allowed.");
+// }
