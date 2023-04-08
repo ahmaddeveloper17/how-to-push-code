b6a6039eaa9076d5e0029e8f770d56461e90d8de
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-08 22:09:30 +0500

    new2

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-// var username= prompt ("Enter your name");
-// document.write("Welcome to my website " + username);
+let Array = ["phisics","urdu","math","chemistry","english","ps","islamiat"]
 
-// let Number = 6 ** 3
-// console.log(Number);
+// Array.push("computer")
+// Array.pop()
+// Array.unshift("computer")
+// Array.shift()
+// Array.length
+// Array.slice(0,2);
+// Array.splice (3,3 , "computer")
 
-// let username = prompt ("Enter your name")
-// document.write = data.reduces ("e")
-// let usernum1 = prompt("enter first number to be added")
-// let usernum2 = prompt("enter second number to be added")
-// let result = usernum1 + usernum2;
-// console.log(result);
-// let table = +prompt ("Which number of table");
-// for (let index = 1; index <= 15; index++) {
-//     console.log( table +" x "+ index + "=" + table*index  );
-    
-// }
 
-// let userage = +prompt ("Enter your age.")
-// if (userage <= 10)
-// {
-//     console.log("Welcome.");
-// }
-// else
-// {
-//     console.log("You are not allowed.");
-// }
+
+
+console.log(Array);
