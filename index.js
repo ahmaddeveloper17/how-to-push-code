4d76e56c840337378bd4b4c05207529651212e18
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-09 18:21:12 +0500

    new2

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let Array = ["phisics","urdu","math","chemistry","english","ps","islamiat"]
-
-// Array.push("computer")
-// Array.pop()
-// Array.unshift("computer")
-// Array.shift()
-// Array.length
-// Array.slice(0,2);
-// Array.splice (3,3 , "computer")
-
-
-
-
-console.log(Array);
+let prevstudents = localStorage.getItem("students");
+let students = prevstudents ? JSON.parse(prevstudents) : [];
+function providestudents() {
+    let std = {
+        Name:prompt("Enter your name"),
+        Fathername:prompt("Enter your father name"),
+        Cnic:+prompt("Enter your cnic"),
+        Class:prompt ("Enter your class"),
+        Phone:+prompt ("Enter your phone number"),
+    }
+    students.push(std);
+    console.log(students);
+    let stringify = JSON.stringify(students);
+    localStorage.setItem("students",stringify);
+}
