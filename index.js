14a8438f6845b481f850db21695df40de06fa387
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-10 18:19:53 +0500

    new2

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-let prevstudents = localStorage.getItem("students");
-let students = prevstudents ? JSON.parse(prevstudents) : [];
-function providestudents() {
-    let std = {
-        Name:prompt("Enter your name"),
-        Fathername:prompt("Enter your father name"),
-        Cnic:+prompt("Enter your cnic"),
-        Class:prompt ("Enter your class"),
-        Phone:+prompt ("Enter your phone number"),
-    }
-    students.push(std);
-    console.log(students);
-    let stringify = JSON.stringify(students);
-    localStorage.setItem("students",stringify);
-}
+// let yourpara = document.getElementById("your-para");
+// // yourpara.innerHTML = ("Hello Ahmad");
+// yourpara.innerHTML = ("<button> Submit </button>")// ,"<h1> hello </h1>" );
+// let mypara = document.getElementById("my-para");
+// mypara.innerHTML = ("<b>ZINDABAD</b>");
+// mypara.style.backgroundColor = ("red");
+// mypara.style.color=("purple");
+// mypara.style.width =" 100%";
+// setInterval(function () {
+    // if (mypara.style.display === "block") {
+    //     mypara.style.display = "none"
+    // }
+    // else{
+    //     mypara.style.display =" block"
+    // }
+//     let width = mypara.style.width ;
+//     if (width === "100%") {
+//         mypara.style.width = "1%"
+//     }
+//     else{
+//         width = +width.replace( "%" ,"");
+//         mypara.style.width = width + 1 + "%" ;
+//     }
+// },10)
+
+// const mypara = document.getElementById("my-para")
+
+const myPara = document.getElementById("my-para");
+myPara.innerHTML = "<b>Pakistan Zindabad</b>";
+setInterval(function () {
+  let color1 =Math.floor(Math.random()*16777215).toString(16);
+  let color2 = Math.floor(Math.random()*16777215).toString(16);
+  myPara.style.backgroundColor = "#" + color1;
+  myPara.style.color = "#" + color2;
+}, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
