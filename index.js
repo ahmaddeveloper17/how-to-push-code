8d94d3cdb33254e9a0bc2f8773b8c449a7501253
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-12 19:56:51 +0500

    new2

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-// let yourpara = document.getElementById("your-para");
-// // yourpara.innerHTML = ("Hello Ahmad");
-// yourpara.innerHTML = ("<button> Submit </button>")// ,"<h1> hello </h1>" );
-// let mypara = document.getElementById("my-para");
-// mypara.innerHTML = ("<b>ZINDABAD</b>");
-// mypara.style.backgroundColor = ("red");
-// mypara.style.color=("purple");
-// mypara.style.width =" 100%";
+
+// const hoursElem = document.getElementById("hours");
+// const minutesElem = document.getElementById("minutes");
+// const secondsElem = document.getElementById("seconds");
+// const amPmElem = document.getElementById("amPm");
+
 // setInterval(function () {
-    // if (mypara.style.display === "block") {
-    //     mypara.style.display = "none"
-    // }
-    // else{
-    //     mypara.style.display =" block"
-    // }
-//     let width = mypara.style.width ;
-//     if (width === "100%") {
-//         mypara.style.width = "1%"
+//   const date = new Date();
+//   let hours = date.getHours();
+//   let amPm = "AM";
+//   if(hours > 11) {
+//     amPm = "PM";
+//     if(hours > 12) {
+//         hours -= 12;
 //     }
-//     else{
-//         width = +width.replace( "%" ,"");
-//         mypara.style.width = width + 1 + "%" ;
-//     }
-// },10)
-
-// const mypara = document.getElementById("my-para")
-
-const myPara = document.getElementById("my-para");
-myPara.innerHTML = "<b>Pakistan Zindabad</b>";
-setInterval(function () {
-  let color1 =Math.floor(Math.random()*16777215).toString(16);
-  let color2 = Math.floor(Math.random()*16777215).toString(16);
-  myPara.style.backgroundColor = "#" + color1;
-  myPara.style.color = "#" + color2;
-}, 500);
+//   }
+//   hoursElem.innerText = hours;
+//   minutesElem.innerText = date.getMinutes();
+//   secondsElem.innerText = date.getSeconds();
+//   amPmElem.innerText = amPm;
+// }, 1000);
 
 
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let ampm = document.getElementById("ampm");
 
 
+setInterval(
+    function () {
+        let date = new Date();
+        let hours = date.getHours();
+        let amPm = "AM";
+          if(hours > 11) {
+            amPm = "PM";
+            if(hours > 12) {
+                hours -= 12;
+            }
+          }
+
+        hours.innerText = hours;
+        minutes.innerText = date.getMinutes();
+        seconds.innerText = date.getSeconds();
+        ampm.innerText = amPm;
 
 
-
-
+    },1000
+);
 
 
 
