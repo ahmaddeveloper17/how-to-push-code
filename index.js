a39c58f2bcb16a77c7712a683d2890334d28723b
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-04-17 00:32:12 +0500

    new2

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
+let hoursElement = document.getElementById("hours");
 
-// const hoursElem = document.getElementById("hours");
-// const minutesElem = document.getElementById("minutes");
-// const secondsElem = document.getElementById("seconds");
-// const amPmElem = document.getElementById("amPm");
+let minutesElement = document.getElementById("minutes");
 
-// setInterval(function () {
-//   const date = new Date();
-//   let hours = date.getHours();
-//   let amPm = "AM";
-//   if(hours > 11) {
-//     amPm = "PM";
-//     if(hours > 12) {
-//         hours -= 12;
-//     }
-//   }
-//   hoursElem.innerText = hours;
-//   minutesElem.innerText = date.getMinutes();
-//   secondsElem.innerText = date.getSeconds();
-//   amPmElem.innerText = amPm;
-// }, 1000);
+let secondsElement = document.getElementById("seconds");
 
-
-let hours = document.getElementById("hours");
-let minutes = document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
-let ampm = document.getElementById("ampm");
-
+let ampmElement = document.getElementById("ampm");
 
 setInterval(
-    function () {
-        let date = new Date();
-        let hours = date.getHours();
-        let amPm = "AM";
-          if(hours > 11) {
-            amPm = "PM";
-            if(hours > 12) {
-                hours -= 12;
-            }
-          }
-
-        hours.innerText = hours;
-        minutes.innerText = date.getMinutes();
-        seconds.innerText = date.getSeconds();
-        ampm.innerText = amPm;
-
-
-    },1000
+  function () {
+    let date = new Date();
+    let currentHours = date.getHours();
+    let amPm = "AM";
+    if (currentHours>11) {
+      amPm = "PM";
+      if (currentHours>12) {
+        currentHours -= 12
+      }
+    }
+    hoursElement.innerHTML = currentHours;
+    minutesElement.innerHTML = date.getMinutes();
+    secondsElement.innerHTML = date.getSeconds();
+    ampmElement.innerHTML = amPm ; 
+  },1000
 );
-
-
-
-
-
-
-
-
