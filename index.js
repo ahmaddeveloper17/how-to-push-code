b4778bebe9555a2e6df52d92de2fd647ee436a3d
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-05-07 14:49:16 +0500

    new2

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// let hoursElement = document.getElementById("hours");
-
-// let minutesElement = document.getElementById("minutes");
-
-// let secondsElement = document.getElementById("seconds");
-
-// let ampmElement = document.getElementById("ampm");
-
-// setInterval(
-//   function () {
-//     let date = new Date();
-//     let currentHours = date.getHours();
-//     let amPm = "AM";
-//     if (currentHours>11) {
-//       amPm = "PM";
-//       if (currentHours>12) {
-//         currentHours -= 12
-//       }
-//     }
-//     hoursElement.innerHTML = currentHours;
-//     minutesElement.innerHTML = date.getMinutes();
-//     secondsElement.innerHTML = date.getSeconds();
-//     ampmElement.innerHTML = amPm ; 
-//   },1000
-// );
+// function color() {
+// document.getElementById("gg").style.backgroundColor="red";
+// }
