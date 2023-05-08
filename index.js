5e194a36d7cb9086b667991a5b3f9db001a41ff9
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-05-08 19:57:16 +0500

    new2

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// function color() {
-// document.getElementById("gg").style.backgroundColor="red";
+//  function over() {
+// document.write("<h1>hello pakistan</h1>");}
+// function out() {
+//    document.write("World Ends Tomorrow");
 // }
+// function over() {
+//    document.getElementById("para").style.backgroundColor= "red"
+// }
+// function out() {
+//    document.getElementById("para").style.backgroundColor= "blue"
+// }
+
+function youtubepage() {
+   window.location= "https://www.youtube.com/"
+}
+function mygithublink() {
+   window.location= "https://github.com/ahmaddeveloper17"
+}
