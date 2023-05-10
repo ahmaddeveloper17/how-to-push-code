9e98a93b064aa4f999544ac1a0c71251b74a9f4d
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-05-10 11:08:26 +0500

    new2

--- index.js
+++ index.js
@@ -10,9 +10,87 @@ function out() {
    document.getElementById("para").className = "changebg2"//style.backgroundColor= "blue"
 }
 
-// function youtubepage() {
-//    window.location= "https://www.youtube.com/"
-// }
-// function mygithublink() {
-//    window.location= "https://github.com/ahmaddeveloper17"
-// }
+function youtubepage() {
+   window.location= "https://www.youtube.com/"
+}
+function mygithublink() {
+   window.location= "https://github.com/ahmaddeveloper17"
+}
+function printpage() {
+   window.print()
+}
+let car = [
+   {
+      color : "red",
+      no : 2015 ,
+      modal : "honda"
+   },
+   {
+      color : "green",
+      no : 2016 ,
+      modal : "tayota"
+   },
+   {
+      color : "blue",
+      no : 2017 ,
+      modal : "honda"
+   },
+   {
+      color : "yellow",
+      no : 2018 ,
+      modal : "bmw"
+   },
+   {
+      color : "gray",
+      no : 2019 ,
+      modal : "honda"
+   },
+   {
+      color : "black",
+      no : 2020 ,
+      modal : "bugatti"
+   },
+   {
+      color : "purple",
+      no : 2021 ,
+      modal : "bmw"
+   },
+   {
+      color : "pink",
+      modal : 2022 ,
+      modal : "honda"
+   },
+      {
+      color : "green",
+      no : 2023,
+      modal : "tayota"
+   },
+   {
+      color : "red",
+      no : 2023 ,
+      modal : "honda"
+   },
+   {
+      color : "purple",
+      no : 2022 ,
+      modal : "honda"
+   },
+   {
+      color : "sky",
+      no : 2022 ,
+      modal : "tayota"
+   },
+   {
+      color : "white",
+      no : 2015 ,
+      modal : "honda"
+   }
+]
+for (let index = 0; index < car.length; index++) {
+   const Element = car[index];
+   if (Element.modal === "tayota") {
+      console.log(Element. modal);
+   }
+}
+
+
